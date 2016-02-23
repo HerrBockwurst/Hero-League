161dc3c1249f054ee4f49bf7828b294aae0c343a
BuildTools <unconfigured@null.spigotmc.org>
2016-02-23 11:47:49 +0100

    Timer

--- src/de/herrbockwurst/heroleague/Shedulars/GameStartCountdown.java
+++ src/de/herrbockwurst/heroleague/Shedulars/GameStartCountdown.java
@@ -1,6 +1,7 @@
 package de.herrbockwurst.heroleague.Shedulars;
 
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
 import de.herrbockwurst.heroleague.Main;
@@ -12,13 +13,24 @@ public class GameStartCountdown extends Main {
 	public void start () {
 		int waiting = 121;
 		
-		BukkitTask starttimer = Bukkit.getScheduler().runTaskTimerAsynchronously(this, new Runnable() {
+		/*BukkitTask starttimer = Bukkit.getScheduler().runTaskTimerAsynchronously(this, new Runnable() {
 			public void run() {
 				if(waiting == 0) {
 					starttimer.cancel();
 				}
 			}
-		} ,0, 20*1);
+		} ,0, 20*1);*/
+		
+		new BukkitRunnable() {
+
+			@Override
+			public void run() {
+				//doStuff
+				this.cancel();
+				
+			}
+			
+		}.runTaskTimerAsynchronously(this, 0, 20);
 	}
 	
 }
